988b9a0017280ba9ff34e1d301c27f2d1b3a8cac
quangthe2104 <quangthe2104@gmail.com>
2013-12-21 21:42:04 +0700

    Add ajax

--- vpw/views.py
+++ vpw/views.py
@@ -122,7 +122,8 @@ def vpw_logout(request):
     return HttpResponseRedirect('/')
 
 def user_profile(request):
-    return render(request, "frontend/user_profile.html")
+    materials = vpr_materials_by_author(request.user)
+    return render(request, "frontend/user_profile.html", {"materials": materials})
 
 def search_result(request):
     keyword = request.REQUEST.get('keyword', '')
