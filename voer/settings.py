6770f87f92ff887517827c51c18440584942d4d9
quangthe2104 <quangthe2104@gmail.com>
2013-12-24 19:48:57 +0700

    Change voer/__init__.pyc

--- voer/settings.py
+++ voer/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'voer_django',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': 3306,
     }
