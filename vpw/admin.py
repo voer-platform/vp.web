a6fcb2b5877790b74453e10a30168cb67e889350
Zniper <me.zniper@gmail.com>
2014-02-27 21:24:32 +0700

    Add RTE to flatpage admin

--- vpw/admin.py
+++ vpw/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.flatpages.admin import FlatpageForm, FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
+## OOPS this is a custom widget that works for initializing
+## tinymce instances on stacked and tabular inlines
+## for flatpages, just use the tinymce packaged one.
+#from content.widgets import TinyMCE
+from tinymce.widgets import TinyMCE
 
-# Register your models here.
+
+class PageForm(FlatpageForm):
+
+    class Meta:
+        model = FlatPage
+        widgets = {
+            'content': TinyMCE(attrs={'cols': 100, 'rows': 15}),
+        }
+
+
+class PageAdmin(FlatPageAdmin):
+    """
+    Page Admin
+    """
+    form = PageForm
+
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, PageAdmin)
