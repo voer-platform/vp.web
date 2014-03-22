d9b2f81040b19d430c92397f4439ed50fbd62cfa
luongtd <luongdhcongnghe49cb@gmail.com>
2014-03-22 17:27:24 +0700

    - show stats for items in profile - show avatar for user in herder

--- vpw/templatetags/vpw_filters.py
+++ vpw/templatetags/vpw_filters.py
@@ -1,5 +1,8 @@
 from django import template
+from django.db.models.query_utils import Q
 from django.template.defaultfilters import stringfilter
+from vpw.models import Material
+from vpw.vpr_api import vpr_materials_by_author, vpr_get_favorite
 
 register = template.Library()
 
@@ -28,3 +31,45 @@ def break_keywords(value):
 def strip(value):
     """ Replace CR-LF in string value by comma character """
     return value.strip()
+
+
+@register.filter
+@stringfilter
+def get_unpublished_count(uid):
+    stats_count = ''
+
+    materials = Material.objects.filter(Q(creator_id=uid) & (Q(version=None) | Q(version=0)))
+    stats_count = len(materials)
+
+    if stats_count:
+        return ' (%s)' % stats_count
+
+    return
+
+
+@register.filter
+@stringfilter
+def get_published_count(pid):
+    stats_count = ''
+
+    published_items = vpr_materials_by_author(pid)
+    stats_count = published_items['count']
+
+    if stats_count:
+        return ' (%s)' % stats_count
+
+    return
+
+
+@register.filter
+@stringfilter
+def get_favorites_count(pid):
+    stats_count = ''
+
+    favorites = vpr_get_favorite(pid)
+    stats_count = favorites['count']
+
+    if stats_count:
+        return ' (%s)' % stats_count
+
+    return
