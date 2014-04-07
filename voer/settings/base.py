78fb3aa6f46d89c76bfa2007b8aacd241627eae6
Luong Trieu <luongdhcongnghe49cb@gmail.com>
2014-04-08 01:29:04 +0700

    revert code

--- voer/settings/base.py
+++ voer/settings/base.py
@@ -59,7 +59,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware', 
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 )
 
 
@@ -173,12 +173,19 @@ VPW_SESSION_MAX_AGE = 1 # days
 
 SITE_URL = 'voer.edu.vn' # URL Site
 
+MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
+MEDIA_URL = '/media/'
+
+TINYMCE_JS_URL = MEDIA_URL + "tiny_mce/tiny_mce.js"
+TINYMCE_JS_ROOT = os.path.join(MEDIA_ROOT, "tiny_mce")
+
 TINYMCE_DEFAULT_CONFIG = {
     'theme': "advanced",
-    'plugins': "autolink,lists,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+    'skin': "cirkuit",
+    'plugins': "autolink,lists,spellchecker,style,layer,table,advhr,advlink,emotions,iespell,latex,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras",
     'theme_advanced_buttons1': "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect,fontsizeselect,|,code,fullscreen,preview",
     'theme_advanced_buttons2': "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,|,forecolor,backcolor",
-    'theme_advanced_buttons3': "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr",
+    'theme_advanced_buttons3': "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,latex,media",
     # 'theme_advanced_buttons4': "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
     'theme_advanced_toolbar_location': "top",
     'theme_advanced_toolbar_align': "left",
@@ -193,8 +200,6 @@ TINYMCE_DEFAULT_CONFIG = {
     'convert_urls': False
 }
 
-MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
-MEDIA_URL = '/media/'
 
 # COMPRESSOR
 
